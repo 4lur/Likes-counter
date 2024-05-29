8b0cdc85ce13790365d5a323d4dd743d2aa31df5
4lur <156508035+4lur@users.noreply.github.com>
2024-05-29 19:15:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    let likeCount = 0;
+
+    const likeButton = document.getElementById('likeButton');
+    const likeCountSpan = document.getElementById('likeCount');
+
+    likeButton.addEventListener('click', () => {
+        likeCount++;
+        likeCountSpan.textContent = likeCount;
+    });
+});
